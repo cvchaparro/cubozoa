aee5a64afd79a6ed74db4bde50cf3b200f55bed3
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2021-10-24 00:30:15 -0500

    Handle imports

--- src/parse.lisp
+++ src/parse.lisp
@@ -20,5 +20,23 @@
 
 ;; TODO: This ultimately doesn't belong here, but for testing it's okay
 (defmethod parse (filespec (type (eql :yaml)) &rest args)
-  (let ((model (apply #'cl-yaml:parse filespec args)))
-    (if (listp model) (rest model) model)))
+  (let* ((model (apply #'cl-yaml:parse filespec args))
+         (models (if (listp model) (rest model) (list model))))
+    (flatten (union models (maybe-import-models filespec models)))))
+
+(defun maybe-import-models (filespec models)
+  "If `MODELS' has any imports, parse and load them."
+  (labels ((parse* (file)
+             (let ((pathspec (make-pathname
+                              :name (pathname-name file)
+                              :type (pathname-type file)
+                              :directory (uiop:merge-pathname-directory-components
+                                          (remove-if (lambda (part) (string= part "."))
+                                                     (pathname-directory file))
+                                          (pathname-directory filespec)))))
+               (parse pathspec t :multi-document-p t))))
+    (ctypecase models
+      (list       (mapcar (lambda (m) (maybe-import-models filespec m)) models))
+      (hash-table (let ((imports (gethash "import" models)))
+                    (remhash "import" models)
+                    (if imports (mapcar #'parse* imports) models))))))
